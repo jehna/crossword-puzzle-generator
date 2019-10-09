346a4599fd64bbfb2525bb47c973dd8ed60fd84e
Jesse Luoto <jesse.luoto@gmail.com>
2019-10-10 01:20:03 +0300

    Add simple crossword puzzle generator
    - Used WordNet (Finnish) for words
    - WordNet parser assumed WordNet location, so forked the code
    - Currently generates only rectangular, small crosswords

--- board.ts
+++ board.ts
@@ -12,8 +12,12 @@ export interface Word {
   direction: Direction,
   word: string,
   x: number,
-  y: number
+  y: number,
+  _data: string[]
 }
+
+const uniq = <T>(input: T[]) => [...new Set(input)]
+const intersectOrInsert = (input: string, current: string) => current === EMPTY ? uniq(input.split('')).join('') : uniq(input.split('')).filter(char => current.indexOf(char) > -1).join('')
 
 const assignArr = <T>(value: T, index: number, arr: T[]): T[] =>
   arr.slice(0, index).concat(value).concat(arr.slice(index + 1))
@@ -21,24 +25,24 @@ const assignArr = <T>(value: T, index: number, arr: T[]): T[] =>
 const get = (x: number,y: number, board: Board) => board._data[y * board.width + x]
 const set = (x: number,y: number, value: string, board: Board) => ({
   ...board,
-  _data: assignArr(value.charAt(0), y * board.width + x, board._data)
+  _data: assignArr(intersectOrInsert(value, get(x, y, board)), y * board.width + x, board._data)
 })
 
 export const getWords = (board: Board) => {
   const allWords = Array<Word>()
   for(let x = 0; x < board.width; x++) {
-    let word = ''
+    let _data = []
     for (let y = 0; y < board.height; y++) {
-      word += get(x, y, board)
+      _data.push(get(x, y, board))
     }
-    allWords.push({ word, x, y: 0, direction: 'vertical' })
+    allWords.push({ word: _data.join(''), x, y: 0, direction: 'vertical', _data })
   }
   for (let y = 0; y < board.height; y++) {
-    let word = ''
+    let _data = []
     for(let x = 0; x < board.width; x++) {
-      word += get(x, y, board)
+      _data.push(get(x, y, board))
     }
-    allWords.push({ word, y, x: 0, direction: 'horizontal' })
+    allWords.push({ word: _data.join(''), y, x: 0, direction: 'horizontal', _data })
   }
   return allWords
 }
@@ -52,8 +56,23 @@ export const setWord = (word: string, x: number, y: number, direction: Direction
       board
     ), board)
 
+export const setPossibleWords = (words: string[], x: number, y: number, direction: Direction, board: Board) => {
+  for (let index = 0; index < (words[0] ? words[0].length : 0); index++) {
+    board = set(
+      direction === 'horizontal' ? x + index : x,
+      direction === 'vertical' ? y + index : y,
+      words.map(w => w.charAt(index)).join(''),
+      board
+    )
+  }
+  return board
+}
+
+
 export const createBoard = (width: number, height: number): Board => ({
   width,
   height,
   _data: Array<string>(width*height).fill(EMPTY)
 })
+
+export const getMatcher = (word: Word) => new RegExp('^' + word._data.map(chars => chars === EMPTY ? '.' : `[${chars}]`).join('') + '$')
--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
-import { createBoard, getWords, setWord } from './board'
+import { createBoard, setWord, getWords, Board } from './board'
+import { experiment } from './solver'
 
-const board = setWord('foo', 1, 0, 'vertical', createBoard(4,4))
+const prettyPrint = (board: Board) => {
+  let result = ''
+  for (let i = 0; i < board.height; i++) {
+    result += board._data.slice(i * board.width, i * board.width + board.width).join('')
+    result += '\n'
+  }
+  return result
+}
 
-console.log(getWords(board))
+async function run() {
+  const board = setWord('sonja', 0, 1, 'horizontal', createBoard(5,5))
+  const result = await experiment(board)
+  console.log(prettyPrint(result))
+}
+
+run()
